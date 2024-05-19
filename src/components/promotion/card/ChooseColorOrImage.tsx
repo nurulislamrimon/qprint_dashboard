3431796b1159a9a63b09bd1483ff1f32fdd8dbac
nurulislamrimon <nurulislamrimon@gmail.com>
2024-05-19 19:53:58 +0600

    issue skipped

--- src/components/promotion/card/ChooseColorOrImage.tsx
+++ src/components/promotion/card/ChooseColorOrImage.tsx
@@ -12,7 +12,7 @@ import {
 const ChooseColorOrImage = ({
   handleChange,
 }: {
-  handleChange: (
+  handleChange?: (
     e: React.ChangeEvent<
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >
